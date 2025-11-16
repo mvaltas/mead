446e0c06ce50fa75378e86ba328a27951ff406e3
Marco Valtas <mavcunha@gmail.com>
2025-11-16 01:09:10 -0800

    Renaming vars

--- mead/models/preypredator.py
+++ mead/models/preypredator.py
@@ -6,19 +6,19 @@ preys = Stock("Preys", initial_value=40)
 predators = Stock("Predators", initial_value=10)
 
 prey_birth_rate = Auxiliary("Prey growth", formula=lambda: 1.2)
-prey_birth_flow = Flow(
+prey_births = Flow(
     "Prey births", 
     formula=lambda: prey_birth_rate.compute() * preys.value
 )
 
 prey_hunt_rate = Auxiliary("Predation rate", lambda: 0.05)
-prey_death_flow = Flow(
+prey_deaths = Flow(
     "Prey deaths",
     formula=lambda: preys.value * (predators.value * prey_hunt_rate.compute()),
 )
 
-preys.add_inflow(prey_birth_flow)
-preys.add_outflow(prey_death_flow)
+preys.add_inflow(prey_births)
+preys.add_outflow(prey_deaths)
 
 predator_death_rate = Auxiliary("Predator death rate", lambda: 1.2)
 predator_deaths = Flow(
