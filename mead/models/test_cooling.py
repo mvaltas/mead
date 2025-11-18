4aa3f36ccc6daaf713bde3a342d5983541c834a6
Marco Valtas <mavcunha@gmail.com>
2025-11-17 19:00:58 -0800

    Test cooling history as well

--- mead/models/test_cooling.py
+++ mead/models/test_cooling.py
@@ -10,7 +10,9 @@ def test_cooling_eulers_method():
     temperature.add_outflow(cooling)
     m = Model()
     m.add_stock(temperature)
-    m.run(steps=8, dt=0.5)
+    dt = 0.5
+    m.run(steps=9, dt=dt)
+    # temperature decay...
     assert temperature.history[0] == approx(100)
     assert temperature.history[1] == approx(75.00)
     assert temperature.history[2] == approx(56.25)
@@ -21,4 +23,14 @@ def test_cooling_eulers_method():
     assert temperature.history[7] == approx(13.35, 0.01)
     assert temperature.history[8] == approx(10.01, 0.01)
 
+    # cooling decay...
+    assert cooling.history[0] * dt == approx(25.00)
+    assert cooling.history[1] * dt == approx(18.75, 0.01)
+    assert cooling.history[2] * dt == approx(14.06, 0.01)
+    assert cooling.history[3] * dt == approx(10.55, 0.01)
+    assert cooling.history[4] * dt == approx(7.91, 0.01)
+    assert cooling.history[5] * dt == approx(5.93, 0.01)
+    assert cooling.history[6] * dt == approx(4.45, 0.01)
+    assert cooling.history[7] * dt == approx(3.34, 0.01)
+    assert cooling.history[8] * dt == approx(2.50, 0.01)
 
