b0f84827edcd9e5032ec543eab7544a0d8796bd6
Marco Valtas <mavcunha@gmail.com>
2025-11-27 14:55:11 -0800

    Example based on fig1-6 Business Dynamics

---
+++ examples/chickens_roadcrossing.py
@@ -0,0 +1,28 @@
+from mead import Model, Stock, Flow, Delay, Auxiliary
+
+with Model("Chickens and Road Crossings", dt=1) as model:
+    chickens = Stock("Chickens", initial_value=10)
+
+    # 60% of the chickens lay eggs
+    egg_lays = Flow("Egg Lays", chickens * 0.6)
+    # it takes 7 days for an egg to hatch
+    brooding = Delay("Brooding", egg_lays, 7)
+    # 80% of the eggs actually hatch
+    egg_hatch = Flow("Egg Hatch", brooding * 0.8)
+    # 80% chickens attempt to cross the road
+    cross_attempts = Auxiliary("Attempt to cross", chickens * 0.8)
+    # 60% of the cross attempts are fatal
+    road_cross = Flow("Fatal Crossing", cross_attempts * 0.6)
+    # new chickens...(instantaneous grown)
+    chickens.add_inflow(egg_hatch)
+    # lost chickens to fatal cross...
+    chickens.add_outflow(road_cross)
+
+
+results = model.run(duration=100)
+model.plot(results, columns = [
+    'Chickens',
+    'Fatal Crossing',
+    #'Egg Lays',
+    #'Egg Hatch',
+    ])
