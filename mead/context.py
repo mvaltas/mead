8042f366317c0d9204ce40b918628a908db28d99
Marco Valtas <mavcunha@gmail.com>
2025-11-26 21:19:34 -0800

    Fix LSP error due lack of type annotation

--- mead/context.py
+++ mead/context.py
@@ -1,5 +1,11 @@
 import contextvars
+from typing import TYPE_CHECKING, Optional
+
+if TYPE_CHECKING:
+    from mead.model import Model
 
 # Context variable to hold the currently active Model instance.
 # Elements will check this variable upon initialization to auto-register.
-current_model = contextvars.ContextVar("current_model", default=None)
+current_model: contextvars.ContextVar[Optional[Model]] = contextvars.ContextVar(
+    "current_model", default=None
+)
